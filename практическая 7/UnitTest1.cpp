34c80675a2b84a1eeb0ac3510cf244ba2785ec93
AnastasiaTsvetkova525 <cvetkova-202020@mail.ru>
2025-12-19 12:46:22 +0300

    тест практ 7

--- практическая 7/UnitTest1.cpp
+++ практическая 7/UnitTest1.cpp
@@ -1,22 +1,16 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "..\task7\практическая 7.cpp"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {
-    TEST_CLASS(PowerRootOperationsTests)
-    {
-    public:
-
-        // Позитивный тест: square
-        TEST_METHOD(TestSquare_Positive)
-        {
-            double value = 3.0;
-            double expected = 9.0;
-            double result = PowerRootOperations::square(value);
-            Assert::AreEqual(expected, result, 0.0001);
-        }
-
-    };
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
 }
