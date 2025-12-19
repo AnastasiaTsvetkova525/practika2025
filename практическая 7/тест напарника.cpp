b2118c65ab237bd2716a5bd8b7106431cd644876
AnastasiaTsvetkova525 <cvetkova-202020@mail.ru>
2025-12-19 13:00:54 +0300

    Update and rename UnitTest1.cpp to тест напарника.cpp

---
+++ практическая 7/тест напарника.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\task7\task7.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(PowerRootOperationsTests)
+    {
+    public:
+
+        // Позитивный тест: square
+        TEST_METHOD(TestSquare_Positive)
+        {
+            double value = 3.0;
+            double expected = 9.0;
+            double result = PowerRootOperations::square(value);
+            Assert::AreEqual(expected, result, 0.0001);
+        }
+
+  
+        }
+    };
+}
